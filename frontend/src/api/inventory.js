da2e1aa449662678c6d3ed9ac3a6f4752071fbb9
Kaushik Plabon <kaushikplabon@Kaushiks-Mac-Pro.local>
2025-08-26 17:11:38 +0600

    35

--- frontend/src/api/inventory.js
+++ frontend/src/api/inventory.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
-const API_URL = import.meta.env.REACT_APP_BACKEND_URL || "http://localhost:5043/api/inventory"
+const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5043/api/inventory"
 
+// Inventory CRUD
 export const getInventories = async () => {
   const res = await axios.get(API_URL, { withCredentials: true })
   return res.data
@@ -24,5 +25,21 @@ export const updateInventory = async (id, data) => {
 
 export const deleteInventory = async (id) => {
   const res = await axios.delete(`${API_URL}/${id}`, { withCredentials: true })
+  return res.data
+}
+
+// Custom ID functions
+export const getCustomIDs = async (inventoryId) => {
+  const res = await axios.get(`${API_URL}/${inventoryId}/custom-ids`, { withCredentials: true })
+  return res.data
+}
+
+export const createCustomID = async (inventoryId, data) => {
+  const res = await axios.post(`${API_URL}/${inventoryId}/custom-ids`, data, { withCredentials: true })
+  return res.data
+}
+
+export const deleteCustomID = async (inventoryId, id) => {
+  const res = await axios.delete(`${API_URL}/${inventoryId}/custom-ids/${id}`, { withCredentials: true })
   return res.data
 }
